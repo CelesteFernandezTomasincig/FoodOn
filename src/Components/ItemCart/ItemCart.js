8c398362b32773064cc6c3b4aff9ab9c69fbe2d3
Celeste Fernandez Tomasincig <celesteft00@gmail.com>
2023-03-30 17:15:10 -0300

    Entrega Final

--- src/Components/ItemCart/ItemCart.js
+++ src/Components/ItemCart/ItemCart.js
@@ -1,22 +1,19 @@
-import React from "react";
 import { useCartContext } from "../../context/CardContext";
-import './itemCart.css';                
-import {ComidaCardDetail} from '../ComidaCardDetail/ComidaCardDetail'
 
+const ItemCart = ({ product }) => {
+  const { removeProduct } = useCartContext();
 
-const ItemCart = ({ dataComida }) =>{
-const {removeProduct}=useCartContext();
-    return (
-        <div className="itemCart">
-            <img src={dataComida.img}></img>
-            <div>
-                <p>Titulo: {dataComida.name}</p>
-                <p>Precio: {dataComida.price}</p>
-                <p>Subtotal: ${dataComida.cantidad * dataComida.price}</p>
-                <button onClick={() => removeProduct(dataComida.id)}>Eliminar</button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="itemCart">
+      <img src={product.img} alt={product.hamburguesa} />
+      <div>
+        <p>Titulo: {product.hamburguesa}</p>
+        <p>Precio: {product.price}</p>
+        <p>Subtotal: ${product.quantity * product.price}</p>
+        <button onClick={() => removeProduct(product.id)}>Eliminar</button>
+      </div>
+    </div>
+  );
+};
 
 export default ItemCart;
--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useCartContext } from '../../context/CardContext';
 import { Link } from 'react-router-dom';
 import ItemCart from '../../Components/ItemCart/ItemCart';
 
-
 const Cart =()=> {
+  const { cart } = useCartContext();
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  // Recorre el carrito y suma el precio de cada producto
+  const calculateTotalPrice = () => {
+    let sum = 0;
+    cart.forEach((product) => {
+      sum += product.price * product.cantidad;
+    });
+    setTotalPrice(sum);
+  };
+
+  // Calcula la suma total cada vez que cambia el carrito
+  React.useEffect(() => {
+    calculateTotalPrice();
+  }, [cart]);
+
   return (
     <>
-
-      <p>No hay elementos en el carrito</p>
-      <Link to="/">Hacer Compras</Link>
+      {cart.length > 0 ? (
+        <div>
+          {cart.map((product) => (
+            <ItemCart key={product.id} product={product} />
+          ))}
+          <div>
+            <p>Total: ${totalPrice}</p>
+            <button>Finalizar compra</button>
+          </div>
+          <Link to="/">Seguir comprando</Link>
+        </div>
+      ) : (
+        <div>
+          <p>No hay elementos en el carrito</p>
+          <Link to="/">Hacer Compras</Link>
+        </div>
+      )}
     </>
   );
 }
 
-export default Cart;
+export default Cart;                    
+
+
